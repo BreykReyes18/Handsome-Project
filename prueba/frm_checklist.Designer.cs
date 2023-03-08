0220f607fe3f3a08cc7511718a43c57a4fdd54ee
Brandon Reyes <kevinrochatitan@gmail.com>
2023-03-07 23:16:06 -0600

    correccion

--- prueba/frm_checklist.Designer.cs
+++ prueba/frm_checklist.Designer.cs
@@ -77,8 +77,8 @@
             this.label_encabezado.AutoSize = true;
             this.label_encabezado.BackColor = System.Drawing.Color.Transparent;
             this.label_encabezado.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_encabezado.ForeColor = System.Drawing.Color.White;
-            this.label_encabezado.Location = new System.Drawing.Point(309, 9);
+            this.label_encabezado.ForeColor = System.Drawing.Color.Black;
+            this.label_encabezado.Location = new System.Drawing.Point(39, 9);
             this.label_encabezado.Name = "label_encabezado";
             this.label_encabezado.Size = new System.Drawing.Size(501, 22);
             this.label_encabezado.TabIndex = 0;
@@ -89,7 +89,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.ForeColor = System.Drawing.Color.Black;
             this.label1.Location = new System.Drawing.Point(24, 67);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(32, 15);
@@ -101,8 +101,8 @@
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(592, 67);
+            this.label2.ForeColor = System.Drawing.Color.Black;
+            this.label2.Location = new System.Drawing.Point(171, 140);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 15);
             this.label2.TabIndex = 2;
@@ -110,7 +110,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(704, 67);
+            this.textBox1.Location = new System.Drawing.Point(283, 140);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(123, 20);
             this.textBox1.TabIndex = 3;
@@ -120,7 +120,7 @@
             this.label3.AutoSize = true;
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.ForeColor = System.Drawing.Color.Black;
             this.label3.Location = new System.Drawing.Point(24, 98);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 15);
@@ -139,8 +139,8 @@
             this.label5.AutoSize = true;
             this.label5.BackColor = System.Drawing.Color.Transparent;
             this.label5.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(29, 125);
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(29, 141);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(85, 22);
             this.label5.TabIndex = 11;
@@ -151,8 +151,8 @@
             this.label4.AutoSize = true;
             this.label4.BackColor = System.Drawing.Color.Transparent;
             this.label4.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(591, 125);
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(23, 445);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(357, 22);
             this.label4.TabIndex = 12;
@@ -170,6 +170,7 @@
             this.label6.AutoSize = true;
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
             this.label6.Location = new System.Drawing.Point(320, 67);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(46, 15);
@@ -181,6 +182,7 @@
             this.label7.AutoSize = true;
             this.label7.BackColor = System.Drawing.Color.Transparent;
             this.label7.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.Black;
             this.label7.Location = new System.Drawing.Point(320, 98);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(104, 15);
@@ -213,7 +215,7 @@
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
             this.pictureBox1.Image = global::prueba.Properties.Resources.Eliminar;
-            this.pictureBox1.Location = new System.Drawing.Point(861, 52);
+            this.pictureBox1.Location = new System.Drawing.Point(440, 125);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(100, 50);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
@@ -276,7 +278,7 @@
             this.label14.AutoSize = true;
             this.label14.BackColor = System.Drawing.Color.Transparent;
             this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label14.ForeColor = System.Drawing.Color.White;
+            this.label14.ForeColor = System.Drawing.Color.Black;
             this.label14.Location = new System.Drawing.Point(30, 182);
             this.label14.Name = "label14";
             this.label14.Size = new System.Drawing.Size(130, 15);
@@ -287,6 +289,7 @@
             // 
             this.checkBox1.AutoSize = true;
             this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
             this.checkBox1.Location = new System.Drawing.Point(261, 389);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(43, 17);
@@ -298,6 +301,7 @@
             // 
             this.checkBox2.AutoSize = true;
             this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.ForeColor = System.Drawing.Color.Black;
             this.checkBox2.Location = new System.Drawing.Point(193, 389);
             this.checkBox2.Name = "checkBox2";
             this.checkBox2.Size = new System.Drawing.Size(47, 17);
@@ -309,6 +313,7 @@
             // 
             this.checkBox3.AutoSize = true;
             this.checkBox3.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox3.ForeColor = System.Drawing.Color.Black;
             this.checkBox3.Location = new System.Drawing.Point(101, 389);
             this.checkBox3.Name = "checkBox3";
             this.checkBox3.Size = new System.Drawing.Size(43, 17);
@@ -320,6 +325,7 @@
             // 
             this.checkBox4.AutoSize = true;
             this.checkBox4.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox4.ForeColor = System.Drawing.Color.Black;
             this.checkBox4.Location = new System.Drawing.Point(33, 389);
             this.checkBox4.Name = "checkBox4";
             this.checkBox4.Size = new System.Drawing.Size(47, 17);
@@ -331,6 +337,7 @@
             // 
             this.label13.AutoSize = true;
             this.label13.BackColor = System.Drawing.Color.Transparent;
+            this.label13.ForeColor = System.Drawing.Color.Black;
             this.label13.Location = new System.Drawing.Point(190, 356);
             this.label13.Name = "label13";
             this.label13.Size = new System.Drawing.Size(167, 13);
@@ -341,6 +348,7 @@
             // 
             this.label12.AutoSize = true;
             this.label12.BackColor = System.Drawing.Color.Transparent;
+            this.label12.ForeColor = System.Drawing.Color.Black;
             this.label12.Location = new System.Drawing.Point(29, 356);
             this.label12.Name = "label12";
             this.label12.Size = new System.Drawing.Size(115, 13);
@@ -351,6 +359,7 @@
             // 
             this.checkBox11.AutoSize = true;
             this.checkBox11.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox11.ForeColor = System.Drawing.Color.Black;
             this.checkBox11.Location = new System.Drawing.Point(260, 326);
             this.checkBox11.Name = "checkBox11";
             this.checkBox11.Size = new System.Drawing.Size(43, 17);
@@ -362,6 +371,7 @@
             // 
             this.checkBox12.AutoSize = true;
             this.checkBox12.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox12.ForeColor = System.Drawing.Color.Black;
             this.checkBox12.Location = new System.Drawing.Point(192, 326);
             this.checkBox12.Name = "checkBox12";
             this.checkBox12.Size = new System.Drawing.Size(47, 17);
@@ -373,6 +383,7 @@
             // 
             this.label11.AutoSize = true;
             this.label11.BackColor = System.Drawing.Color.Transparent;
+            this.label11.ForeColor = System.Drawing.Color.Black;
             this.label11.Location = new System.Drawing.Point(190, 295);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(160, 13);
@@ -383,6 +394,7 @@
             // 
             this.label10.AutoSize = true;
             this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.ForeColor = System.Drawing.Color.Black;
             this.label10.Location = new System.Drawing.Point(29, 295);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(95, 13);
@@ -394,7 +406,7 @@
             this.label9.AutoSize = true;
             this.label9.BackColor = System.Drawing.Color.Transparent;
             this.label9.Font = new System.Drawing.Font("Times New Roman", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.ForeColor = System.Drawing.Color.Black;
             this.label9.Location = new System.Drawing.Point(29, 257);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(310, 17);
@@ -406,7 +418,7 @@
             this.label8.AutoSize = true;
             this.label8.BackColor = System.Drawing.Color.Transparent;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.ForeColor = System.Drawing.Color.Black;
             this.label8.Location = new System.Drawing.Point(29, 216);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(138, 15);
@@ -424,6 +436,7 @@
             // 
             this.chbx_cam_mal.AutoSize = true;
             this.chbx_cam_mal.BackColor = System.Drawing.Color.Transparent;
+            this.chbx_cam_mal.ForeColor = System.Drawing.Color.Black;
             this.chbx_cam_mal.Location = new System.Drawing.Point(100, 326);
             this.chbx_cam_mal.Name = "chbx_cam_mal";
             this.chbx_cam_mal.Size = new System.Drawing.Size(43, 17);
@@ -435,6 +448,7 @@
             // 
             this.chbx_cam_bien.AutoSize = true;
             this.chbx_cam_bien.BackColor = System.Drawing.Color.Transparent;
+            this.chbx_cam_bien.ForeColor = System.Drawing.Color.Black;
             this.chbx_cam_bien.Location = new System.Drawing.Point(32, 326);
             this.chbx_cam_bien.Name = "chbx_cam_bien";
             this.chbx_cam_bien.Size = new System.Drawing.Size(47, 17);
@@ -447,8 +461,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.BackgroundImage = global::prueba.Properties.Resources._6548;
-            this.ClientSize = new System.Drawing.Size(1044, 689);
+            this.ClientSize = new System.Drawing.Size(610, 689);
             this.Controls.Add(this.btn_reporte_cam);
             this.Controls.Add(this.btn_actualizar_cam);
             this.Controls.Add(this.btn_limpiar_cam);
@@ -485,7 +498,7 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label_encabezado);
-            this.ForeColor = System.Drawing.Color.White;
+            this.ForeColor = System.Drawing.Color.Black;
             this.Name = "frm_checklist";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "frm_checklist";
